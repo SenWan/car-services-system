0dfb5bb5e69583b14dbef1d8b1ed12242d6c15d7
U Sen Wan <senwan96@gmail.com>
2022-05-08 17:28:37 +0600

    Conditional css class and conditional disabled

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init'
@@ -7,6 +7,7 @@ import SocialLogin from '../SocialLogin/SocialLogin';
 
 
 const Register = () => {
+    const [agree, setAgree] = useState(false);
     const [
         createUserWithEmailAndPassword,
         user,
@@ -31,8 +32,12 @@ const Register = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
+        /* const agree = event.target.terms.checked; */
+        if(agree){
+            createUserWithEmailAndPassword(email, password);
+        }
         createUserWithEmailAndPassword(email, password)
-    }
+    } 
     return (
         <div className='container register-form'>
             <h2 className='text-center mt-3'>Please Register</h2>
@@ -40,9 +45,9 @@ const Register = () => {
                 <input type="text" name="name" id="" placeholder='Your Name' required />
                 <input type="email" name="email" id="" placeholder='Email Address' required />
                 <input type="password" name="password" id="" placeholder='Password' required />
-                <input type="checkbox" name="terms" id="terms" />
-                <label htmlFor="terms">Accept Terms an Condition</label>
-                <input className='w-50 mx-auto btn my btn-primary mt-2' type="submit" value="Register" />
+                <input onClick={() => setAgree(!agree)} type="checkbox" name="terms" id="terms" />
+                <label className={agree ? 'ps-2 text-primary' : 'ps-2 text-danger'} htmlFor="terms">Accept Terms an Condition</label>
+                <input disabled={!agree} className='w-50 mx-auto btn my btn-primary mt-2' type="submit" value="Register" />
             </form>
             <p className='text-center'>Already have an account? <span  style={{cursor:'pointer'}} className='text-primary' onClick={navigateLogin}> Please Login !</span></p>
             <SocialLogin></SocialLogin>
